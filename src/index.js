c52a8fc9d74b46bf517dfabf9847218ff7fc037f
Thiago Santos <thiagop.santos182@gmail.com>
2022-05-05 18:13:11 -0400

    Challenge concluded

--- src/index.js
+++ src/index.js
@@ -10,19 +10,79 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({
+      error: 'Username not found'
+    })
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  const MAX_TODO_PER_FREE_USER = 10;
+
+  if (!user.pro && user.todos.length === MAX_TODO_PER_FREE_USER) {
+    return response.status(403).json({ 
+      error: 'User reached free todos limit'
+    });
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found' });
+  }
+
+  if(!validate(id)) {
+    return response.status(400).json({
+      error: 'Please inform a todo key valid'
+    });
+  }
+  
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({
+      error: 'Todo not found'
+    })
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({ 
+      error: 'User not found' 
+    });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
